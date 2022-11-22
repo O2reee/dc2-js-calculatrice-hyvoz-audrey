b5a314f80599aff2b1e3f57e2a23ceb8309ec8df
Audrey Hyvoz <audrey.hyvoz@gmail.comgit config --global user.email audrey.hyvoz@gmail.com>
2022-11-22 11:26:08 +0100

    fin de seance

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("salut")
+
+function add(a, b){ 
+	return a + b;
+}
+
+let result = add(1, 1);
+let result = add(1, -1);
+
+
+console.log(result)
+
+function sup(a, b){ 
+	return a - b;
+}
+
+let result = sup(1, 1);
+
+console.log(result)
+
